d8c18537272e914a70d8d27e36b042d5e876dcc9
Kish Dubey <dkish2000@gmail.com>
2020-11-17 21:37:44 -0500

    schedule extraction working

--- schedule.py
+++ schedule.py
@@ -9,8 +9,8 @@ class Course:
         self.end_time = end_time
         self.location = location
 
-    def __repr__():
-        return f'{self.title} {self.type} {self.start_time}-{self.end_time} {self.location}'
+    def __repr__(self):
+        return f'Title: {self.title} \n\n Type: {self.type} \n\n Time: {self.start_time}-{self.end_time} \n\n Location: {self.location}'
 
 image = cv2.imread('data/schedule.png')
 
@@ -18,8 +18,9 @@ extracted_info = pytesseract.image_to_string(image).strip().replace("\n\n", "\n"
 
 extracted_info_list = list(extracted_info.split("\n"))
 
-#for item in extracted_info_list:
-#    print(item)
+for item in extracted_info_list:
+    if item == " ":
+        extracted_info_list.remove(item)
 
 weekdays = set(["monday", "tuesday", "wednesday", "thursday", "friday"])
 
@@ -27,9 +28,10 @@ courses = []
 
 i = 0
 while i < len(extracted_info_list):
-    item = extracted_info_list[i].lower()
+    item = extracted_info_list[i]
     # if weekday
-    if item in weekdays:
+    if item.lower() in weekdays:
+        print(item)
         courses.append("\n")
 
         course_title = extracted_info_list[i+2].strip()
@@ -41,13 +43,7 @@ while i < len(extracted_info_list):
 
         course = Course(course_title, course_type, course_start_time, course_end_time, course_location)
         courses.append(course)
-
-        print(course_title, course_type, course_start_time, course_end_time, course_location)
-
         i += 6
-    # if empty
-    elif item == "\n":
-        i += 1
 
     # else if next course of same day
     else:
@@ -60,6 +56,7 @@ while i < len(extracted_info_list):
 
         course = Course(course_title, course_type, course_start_time, course_end_time, course_location)
         courses.append(course)
-
-        print(course_title, course_type, course_start_time, course_end_time, course_location)
         i += 4
+
+for course in courses:
+    print(course)

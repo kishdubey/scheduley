382469bb5e8cc7e41525e72d165871b018a09e30
Kish Dubey <dkish2000@gmail.com>
2020-11-21 21:57:17 -0500

    generating calendar succesfully

--- generate_calendar.py
+++ generate_calendar.py
@@ -2,15 +2,15 @@
 generating ICS file calendar with course contents
 """
 
-from icalendar import Calendar, Event
-import recurring_ical_events
-from datetime import datetime
+from icalendar import Calendar, Event, vText
+import datetime
 from extract_course_data import get_schedule
+from datetime import time
 
 PATH = 'data/schedule1.png'
 
 classes = get_schedule(PATH)
-ACADEMIC_TERMS = {"Summer": "01/05 - 31/08", "Fall": "01/09 - 31/12", "Winter": "01/01 - 30/04"}
+ACADEMIC_TERMS = {"summer": "01/05 - 31/08", "fall": "01/09 - 31/12", "winter": "01/01 - 30/04"}
 WEEKDAYS = {"monday": 0, "tuesday": 1, "wednesday": 2, "thursday": 3, "friday": 4}
 
 def get_academic_term(academic_term):
@@ -32,13 +32,36 @@ def next_weekday(d, weekday):
 
 def calendar(academic_term, year, classes):
     cal = Calendar()
+    academic_term = academic_term.lower()
     term_start, term_end = get_academic_term(academic_term)
-    term_start_date = datetime.date(year, term_start[3:], term_start[0:2])
-    first_start_date = next_weekday(term_start_date, WEEKDAYS[class.day])
+    term_start_date = datetime.date(year, int(term_start[3:]), int(term_start[0:2]))
 
-    for class in classes:
+    for course in classes:
         event = Event()
-        event.add('summary', f"{class.title} - {class.type}")
+        event.add('summary', f"{course.title} - {course.type}".strip().replace("  ", " "))
 
-        start_date = datetime(year, first_start_date, class.start_time, class.end_time)
-        event.add('dtstart', date =
+        first_start_date = next_weekday(term_start_date, WEEKDAYS[course.day.lower()])
+        hour, minute = int(course.start_time[:2]), int(course.start_time[3:])
+        start_time = time(hour, minute, 0)
+
+        start_date = datetime.datetime.combine(first_start_date, start_time)
+        event.add('dtstart', start_date)
+
+        hour, minute = int(course.end_time[:2]), int(course.end_time[3:])
+        end_time = time(hour, minute, 0)
+
+        end_date = datetime.datetime.combine(first_start_date, end_time)
+        event.add('dtend', end_date)
+
+        last_date = datetime.date(year, int(term_end[3:]), int(term_end[0:2]))
+        course_end = datetime.datetime.combine(last_date, end_time)
+
+        event.add('rrule', {'freq': 'weekly', 'interval': 1, 'byday': f'{course.day[:2]}', 'until': course_end})
+        event['location'] = vText(course.location)
+
+        cal.add_component(event)
+
+        with open(f'ex.ics', 'wb') as ics:
+            ics.write(cal.to_ical())
+
+calendar('Summer', 2020, classes)

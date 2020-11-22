435043abefab8a60fb9d309b1d12616dd540da6c
Kish Dubey <dkish2000@gmail.com>
2020-11-22 16:06:42 -0500

    input form getting to backend

--- scheduley.py
+++ scheduley.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/return_calendar_schedule", methods=['POST'])
+def return_calendar_schedule():
+    file = request.form['scheduleFile']
+    term = request.form['term']
+    year = request.form['year']
+
+    print(year)
+    return year
 
 if __name__ == "__main__":
     app.run(debug=True)

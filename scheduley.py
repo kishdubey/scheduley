313bd2673ba5fcdd96a1ffd0506bb403493494b6
Kish Dubey <dkish2000@gmail.com>
2020-11-22 20:52:22 -0500

    file being saved

--- scheduley.py
+++ scheduley.py
@@ -1,18 +1,36 @@
-from flask import Flask, render_template, request, url_for
-app = Flask(__name__)
+import os
+from os.path import join, dirname, realpath
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from generate_calendar import create_calendar
 
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/image/uploads/')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/return_calendar_schedule", methods=['POST'])
-def return_calendar_schedule():
-    file = request.form['scheduleFile']
-    term = request.form['term']
-    year = request.form['year']
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-    print(year)
-    return year
+@app.route("/return_calendar_schedule", methods=['GET', 'POST'])
+def return_calendar_schedule():
+    if request.method == 'POST':
+        term = request.form['term']
+        year = request.form['year']
+        if request.files:
+            image = request.files["scheduleFile"]
+
+            if image and allowed_file(image.filename):
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            create_calendar(term, year, os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    return render_template("calendar.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

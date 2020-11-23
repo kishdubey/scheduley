4ff93e761268f7f78b4d6e8ca940db333c019b2f
Kish Dubey <dkish2000@gmail.com>
2020-11-23 00:37:45 -0500

    gunicorn not installed

--- app.py
+++ app.py
@@ -16,19 +16,20 @@ def index():
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/landing.html", methods=['POST'])
+@app.route("/landing.html", methods=['GET', 'POST'])
 def create_calendar_schedule():
-    term = request.form['term']
-    year = int(request.form['year'])
-    if request.files:
-        image = request.files["scheduleFile"]
+    if request.method == 'POST':
+        term = request.form['term']
+        year = int(request.form['year'])
+        if request.files:
+            image = request.files["scheduleFile"]
 
-        if image and allowed_file(image.filename):
-            filename = secure_filename(image.filename)
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            if image and allowed_file(image.filename):
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        create_calendar(term, year, os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return render_template('landing.html', schedule=os.path.join(app.config['UPLOAD_FOLDER'], "timetable.ics"))
+            create_calendar(term, year, os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('landing.html', schedule=os.path.join(app.config['UPLOAD_FOLDER'], "timetable.ics"))
 
     return render_template("index.html")
 
